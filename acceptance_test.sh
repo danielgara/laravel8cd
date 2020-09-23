51c4bf2cb4287f4f30e96b9e89ef064e200346ed
DanielGara <yo@danielgara.com>
2020-09-23 17:52:21 -0500

    sh test

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-test $(curl $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' laravel8cd)/public/sum/7/8) -eq 6
+res=$(curl -s -w "%{http_code}" $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' laravel8cd)/public/sum/4/2)
+body=${res::-3}
+if [ $body != "6" ]; then
+  echo "Error"
+fi
